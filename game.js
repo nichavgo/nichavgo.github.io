972e9590624d86f1dca1ebd36622890bb0c5524f
Chavas <nichavgo@yahoo.com.mx>
2021-02-08 17:49:33 -0500

    Basic offline setup.

--- game.js
+++ game.js
@@ -1,15 +1,55 @@
 // Game setup
-let startGame = false;
+function setup(){
+    const setupContainer = document.querySelector('.setup');
+    const onlineButton = document.querySelector('.onlineButton');
+    const offlineButton = document.querySelector('.offlineButton');
+    
+    setupContainer.style.display = 'block';
+    
+    onlineButton.addEventListener('click', function(){ alert('¡Esta función todavía no puede usarse!'); });
+    offlineButton.addEventListener('click', function(){
+        const buttonContainer = document.querySelector('.selectOnOff');
+        buttonContainer.style.display = 'none';
+    
+        setupOfflineGame(setupContainer);
+    });
+    
+    function setupOfflineGame(setupContainer){
+        const offlineSetup = document.querySelector('.offline');
+        offlineSetup.style.display = 'block';
 
-const setupContainer = document.querySelector('.setup');
-const onlineButton = document.querySelector('.onlineButton');
-const offlineButton = document.querySelector('.offlineButton');
+        const playerNameInput = document.querySelector('#playerName');
+        const playerNumberInput = document.querySelector('#playerNumber');
+        const maxPointsInput = document.querySelector('#maxPoints');
+        const playButton = document.querySelector('#playButton');
+        
+        playButton.addEventListener('click', function(){
+            let playerName = playerNameInput.value;
+            if(playerName === ""){ 
+                alert('Tienes que escoger un nombre de jugador.');
+                return;
+            }
+            let numberOfPlayers = Number(playerNumberInput.value);
+            if(numberOfPlayers < 2){
+                alert('Tienes que escoger al menos dos jugadores.');
+                return;
+            }
+            let maxPoints = Number(maxPointsInput.value);
+            if(maxPoints < 1){
+                alert('El valor mínimo para el máximo de puntos es 1.');
+                return;
+            }
 
-setupContainer.style.display = 'block';
+            setupContainer.style.display = 'none';
 
-onlineButton.addEventListener('click', function(){ alert('¡Esta función todavía no puede usarse!'); });
-offlineButton.addEventListener('click', setupOfflineGame);
-
-function setupOfflineGame(){
-
+            startGame(false, false, playerName, numberOfPlayers, maxPoints);
+        })
+    }
 }
+
+function startGame(online, host, playerName, numberOfPlayers, maxPoints){
+    const gameContainer = document.querySelector('.game');
+    gameContainer.style.display = '';
+}
+
+setup();
